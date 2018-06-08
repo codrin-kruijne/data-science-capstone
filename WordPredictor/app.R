e58e594f3b94774f2d5e0ba70eadfeacb139568a
Codrin Kruijne <codrin.kruijne@gmail.com>
2018-06-08 23:06:41 +0200

    First rough draft working on ShinyApps.io

--- WordPredictor/app.R
+++ WordPredictor/app.R
@@ -1,3 +1,5 @@
+# 
+
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -24,14 +26,40 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$processed <- renderText({
-     input$text
-   })
+
+  # Reading in the lookup data
   
-   output$prediction <- renderText({
-      "PREDICTED!!"
-   })
+  bi_lookup <- readRDS("data/server_lookup.rds")
+
+
+  # Prediction function for looking up predictions in saved tables
+  
+  predictWord <- function(string, bi_lookup) {
+   
+    str(bi_lookup)
+    bi_df <- as.data.frame(bi_lookup)
+    prediction <- bi_df[1, ]$variable
+    print(paste("Prediction! ", prediction))
+    prediction
+    
+  }
+  
+  # Display what text was used for the prediction
+  output$processed <- renderText({
+  
+    req(input$text)
+    input$text
+  
+  })
+  
+  # Retrieve most likely next word
+  output$prediction <- renderText({
+    
+    req(input$text)
+    predicted <- predictWord(input$text, bi_lookup)
+    print(paste("Predicted! ", predicted))
+  
+  })
 }
 
 # Run the application 
